d351a6f46b32a1d04d6555e895c204f6630c5960
Gloriarg <gloriaramos.ciclos@gmail.com>
2016-11-29 15:57:42 +0100

    dni

---
+++ colores.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+      if  (A == "verdadero")
+{
+      if (B == "verdadero")  }
+{
+      if (c == "verdadero");
+
+      else (False);
+                  }
+      printf("\nOpciones:\n\n"
+           "\t1.-rojo\n"
+           "\t2.- amarillo"
+           "\t3.- azul"
+           "\t4.- morado"
+           "t5.- verde"
+           )
+return EXIT_SUCCESS;
+}
+
